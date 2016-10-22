22a79502ed47484b421b7d007a2b0a9f2a9572a8
Alexandre Chaussier <a.chaussier@infopen.pro>
2016-10-22 17:44:46 +0200

    Add basic tests

--- tests/testinfra/test_installation.py
+++ tests/testinfra/test_installation.py
@@ -9,8 +9,27 @@ pytestmark = pytest.mark.docker_images(
     'infopen/ubuntu-xenial-ssh-py27:0.2.0'
 )
 
-def test_foo_a(User):
-    assert User().name == 'root'
 
-def test_foo_b(User):
-    assert User().name == 'root'
+def test_packages(SystemInfo, Package):
+
+    if SystemInfo.codename == 'trusty':
+        assert Package('php5-fpm').is_installed
+
+    elif SystemInfo.codename == 'xenial':
+        assert Package('php-fpm').is_installed
+
+
+def test_main_config_file(SystemInfo, File):
+
+    cfg_file_path = ''
+
+    if SystemInfo.codename == 'trusty':
+        cfg_file_path = '/etc/php5/fpm/php-fpm.conf'
+    elif SystemInfo.codename == 'xenial':
+        cfg_file_path = '/etc/php/7.0/fpm/php-fpm.conf'
+
+    cfg_file = File(cfg_file_path)
+
+    assert cfg_file.exists
+    assert cfg_file.is_file
+    assert cfg_file.contains('[global]')
